704a106ac95756de9202d9cdb2a3bad37112ed03
OlenaKut <cundrochka@gmail.com>
2025-05-27 13:41:32 +0200

    Added ViewTests Index and Details

---
+++ EmployeesApp.Web.Tests/EmployeesControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using EmployeesApp.Application.Employees.Interfaces;
+using EmployeesApp.Domain.Entities;
+using EmployeesApp.Web.Controllers;
+using EmployeesApp.Web.Views.Employees;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+
+namespace EmployeesApp.Web.Tests
+{
+    public class EmployeesControllerTests
+    {
+        [Fact]
+        public void Index_NoParams_ReturnsViewResult()
+        {
+            // Arrage
+            var employeeService = new Mock<IEmployeeService>();
+            employeeService
+                .Setup(o => o.GetAll())
+                .Returns(
+                [
+                    new Employee { Id = 1, Name = "Oliver" },
+                    new Employee { Id = 2, Name = "Pontus" }
+                ]);
+
+            //Act
+            var controller = new EmployeesController(employeeService.Object);
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact]
+
+        public void Details_ValidId_ReturnsViewResultWithEmployee()
+        {
+            // Arrage
+            var employeeService = new Mock<IEmployeeService>();
+            employeeService
+                .Setup(o => o.GetById(1))
+                .Returns(new Employee { Id = 1, Name = "Oliver" });
+
+            //Act
+            var controller = new EmployeesController(employeeService.Object);
+
+            // Assert
+            var viewResult = Assert.IsType<ViewResult>(controller.Details(1));
+            Assert.NotNull(viewResult.Model);
+            
+
+        }
+
+    }
+}
